3f01cd547ec58c64b40dde82363d6dea555eb839
Sung Wung, Yeom <yeom4032yeom4032@gmail.com>
2018-12-08 02:13:28 +0900

    Delete loop_1.R

--- R_code/loop_1.R
+++
@@ -1,9 +0,0 @@
-#apply()
-n =100000
-m =100
-A = matrix(rnorm(n*m,100,15),n,m)
-tim = proc.time()
-a.1 = apply(A,1,max)
-proc.time() - time
-windows(height=5, width=6)
-hist(a.1, xlim=c(120,180),xlab="x.max",main="Histogram of Maximums")
